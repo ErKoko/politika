6ab020196cc574e4d027fea2b22bebd459852962
Adrían De Haro <zoyerkoko@gmail.com>
2024-11-16 22:55:12 +0100

    Updates

--- assets/IndexPage.da545041.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,Q as r}from"./plugin-vue_export-helper.f3b44bb8.js";import{_ as t,$ as a}from"./index.2da7f985.js";import"./render.0adcf242.js";const c={};function o(s,n){return t(),a(r,{class:"row items-center justify-evenly"})}var m=e(c,[["render",o]]);export{m as default};
